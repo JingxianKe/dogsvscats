9a6c54e486ad349302ce3cee1d2a75329aab8fbe
shaonianwangyisheng <102451740+shaonianwangyisheng@users.noreply.github.com>
2022-12-30 20:06:11 +0800

    Update dogs_vs_cats_config.py

--- dogs_vs_cats/dogs_vs_cats_config.py
+++ dogs_vs_cats/dogs_vs_cats_config.py
@@ -10,15 +10,15 @@ NUM_TEST_IMAGES = 1250 * NUM_CLASSES
 
 # define the path to the output training, validation, and testing
 # HDF5 files
-TRAIN_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/train.hdf5"
-VAL_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
-TEST_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
+TRAIN_HDF5 = "/kaggle/input/dogs-vs-cats-hdf5/train.hdf5"
+VAL_HDF5 = "/kaggle/input/dogs-vs-cats-hdf5/val.hdf5"
+TEST_HDF5 = "/kaggle/input/dogs-vs-cats-hdf5/test.hdf5"
 
 # path to the output model file
 MODEL_PATH = "output/alexnet_dogs_vs_cats.model"
 
 # define the path to the dataset mean
-DATASET_MEAN = "output/dogs_vs_cats_mean.json"
+DATASET_MEAN = "/kaggle/input/dogs-vs-cats-hdf5/dogs_vs_cats_mean.json"
 
 # define the path to the output directory used for storing plots,
 # classification reports, etc.
