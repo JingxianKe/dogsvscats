d6b5cf3ff810aa82a6a360869c291e428fb1b5d4
shaonianwangyisheng <102451740+shaonianwangyisheng@users.noreply.github.com>
2023-01-31 10:33:41 +0800

    Update crop_accuracy.py

--- dogs_vs_cats/crop_accuracy.py
+++ dogs_vs_cats/crop_accuracy.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 import dogs_vs_cats_config as config
 from imagetoarraypreprocessor import ImageToArrayPreprocessor
 from simplepreprocessor import SimplePreprocessor
@@ -11,63 +10,57 @@ import numpy as np
 import progressbar
 import json
 
-# load the RGB means for the training set
+# 加载训练集的RGB均值
 means = json.loads(open(config.DATASET_MEAN).read())
 
-# initialize the image preprocessors
+# 初始化图像预处理
 sp = SimplePreprocessor(227, 227)
 mp = MeanPreprocessor(means["R"], means["G"], means["B"])
 cp = CropPreprocessor(227, 227)
 iap = ImageToArrayPreprocessor()
 
-# load the pretrained network
+# 加载预训练网络
 print("[INFO] loading model...")
 model = load_model(config.MODEL_PATH)
 
-# initialize the testing dataset generator, then make predictions on
-# the testing data
+# 初始化测试集生成器，进行预测
 print("[INFO] predicting on test data (no crops)...")
 testGen = HDF5DatasetGenerator(config.TEST_HDF5, 64,
 							   preprocessors=[sp, mp, iap], classes=2)
 predictions = model.predict_generator(testGen.generator(),
 									  steps=testGen.numImages // 64, max_queue_size=64 * 2)
 
-# compute the rank-1 and rank-5 accuracies
+# 计算rank-1和rank-5准确率
 (rank1, _) = rank5_accuracy(predictions, testGen.db["labels"])
 print("[INFO] rank-1: {:.2f}%".format(rank1 * 100))
 testGen.close()
 
-# re-initialize the testing set generator, this time excluding the
-# ‘SimplePreprocessor‘
+# 重新初始化测试集生成器，这次不包括‘SimplePreprocessor‘
 testGen = HDF5DatasetGenerator(config.TEST_HDF5, 64,
 							   preprocessors=[mp], classes=2)
 predictions = []
 
-# initialize the progress bar
+# 显示进度条
 widgets = ["Evaluating: ", progressbar.Percentage(), " ",
 		   progressbar.Bar(), " ", progressbar.ETA()]
 pbar = progressbar.ProgressBar(maxval=testGen.numImages // 64,
 							   widgets=widgets).start()
 
-# loop over a single pass of the test data
 for (i, (images, labels)) in enumerate(testGen.generator(passes=1)):
-	# loop over each of the individual images
 	for image in images:
-		# apply the crop preprocessor to the image to generate 10
-		# separate crops, then convert them from images to arrays
+		# 使用crop preprocessor生成10个separate crops，之后把图像转换为数组
 		crops = cp.preprocess(image)
 		crops = np.array([iap.preprocess(c) for c in crops],
 						 dtype="float32")
 		
-		# make predictions on the crops and then average them
-		# together to obtain the final prediction
+		# 预测
 		pred = model.predict(crops)
 		predictions.append(pred.mean(axis=0))
 		
-	# update the progress bar
+	# 进度条更新
 	pbar.update(i)
 
-# compute the rank-1 accuracy
+# 计算rank-1准确率
 pbar.finish()
 print("[INFO] predicting on test data (with crops)...")
 (rank1, _) = rank5_accuracy(predictions, testGen.db["labels"])
