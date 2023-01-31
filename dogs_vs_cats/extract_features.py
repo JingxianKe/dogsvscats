e24da1fca815dee467f5441234d51dacb5858957
shaonianwangyisheng <102451740+shaonianwangyisheng@users.noreply.github.com>
2023-01-31 10:51:57 +0800

    Update extract_features.py

--- dogs_vs_cats/extract_features.py
+++ dogs_vs_cats/extract_features.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications import imagenet_utils
 from tensorflow.keras.preprocessing.image import img_to_array
@@ -12,7 +11,6 @@ import argparse
 import random
 import os
 
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
                 help="path to input dataset")
@@ -24,79 +22,58 @@ ap.add_argument("-s", "--buffer-size", type=int, default=1000,
                 help="size of feature extraction buffer")
 args = vars(ap.parse_args())
 
-# store the batch size in a convenience variable
 bs = args["batch_size"]
 
-# grab the list of images that we’ll be describing then randomly
-# shuffle them to allow for easy training and testing splits via
-# array slicing during training time
 print("[INFO] loading images...")
 imagePaths = list(paths.list_images(args["dataset"]))
 random.shuffle(imagePaths)
 
-# extract the class labels from the image paths then encode the
-# labels
 labels = [p.split(os.path.sep)[-1].split(".")[0] for p in imagePaths]
 le = LabelEncoder()
 labels = le.fit_transform(labels)
 
-# load the ResNet50 network
+# 加载ResNet50网络
 print("[INFO] loading network...")
 model = ResNet50(weights="imagenet", include_top=False)
 
-# initialize the HDF5 dataset writer, then store the class label
-# names in the dataset
 dataset = HDF5DatasetWriter((len(imagePaths), 2048 * 7 * 7),
                             args["output"], dataKey="features", bufSize=args["buffer_size"])
 dataset.storeClassLabels(le.classes_)
 
-# initialize the progress bar
+# 显示进度条
 widgets = ["Extracting Features: ", progressbar.Percentage(), " ",
            progressbar.Bar(), " ", progressbar.ETA()]
 pbar = progressbar.ProgressBar(maxval=len(imagePaths),
                                widgets=widgets).start()
 
-# loop over the images in batches
 for i in np.arange(0, len(imagePaths), bs):
-    # extract the batch of images and labels, then initialize the
-    # list of actual images that will be passed through the network
-    # for feature extraction
     batchPaths = imagePaths[i:i + bs]
     batchLabels = labels[i:i + bs]
     batchImages = []
 
-    # loop over the images and labels in the current batch
     for (j, imagePath) in enumerate(batchPaths):
-        # load the input image using the Keras helper utility
-        # while ensuring the image is resized to 224x224 pixels
+        # 加载输入图像，并确保其大小为224x224像素
         image = load_img(imagePath, target_size=(224, 224))
         image = img_to_array(image)
 
-        # preprocess the image by (1) expanding the dimensions and
-        # (2) subtracting the mean RGB pixel intensity from the
-        # ImageNet dataset
+        # 预处理图像：增加一个维度；提取ImageNet数据集的RGB均值
         image = np.expand_dims(image, axis=0)
         image = imagenet_utils.preprocess_input(image)
 
-        # add the image to the batch
         batchImages.append(image)
 
-    # pass the images through the network and use the outputs as
-    # our actual features
+    # 将图像传进网络，其输出作为我们想要的特征
     batchImages = np.vstack(batchImages)
     features = model.predict(batchImages, batch_size=bs)
 
-    # reshape the features so that each image is represented by
-    # a flattened feature vector of the ‘MaxPooling2D‘ outputs
+    # reshape特征，使得每一个图像都是展平的‘MaxPooling2D‘输出的特征向量
     features = features.reshape((features.shape[0], 2048 * 7 * 7))
 
-    # add the features and labels to our HDF5 dataset
+    # 特征和标签添加进HDF5数据集
     dataset.add(features, batchLabels)
     pbar.update(i)
 
-# close the dataset
+# 关闭数据集
 dataset.close()
 pbar.finish()
-
-
 
