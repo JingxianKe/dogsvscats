ea1c1471b8c3661ed2eb40712558e6b443b107df
shaonianwangyisheng <102451740+shaonianwangyisheng@users.noreply.github.com>
2023-01-31 10:58:23 +0800

    Update hdf5datasetgenerator.py

--- dogs_vs_cats/hdf5datasetgenerator.py
+++ dogs_vs_cats/hdf5datasetgenerator.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from keras.utils import np_utils
 import numpy as np
 import h5py
@@ -6,71 +5,56 @@ import h5py
 class HDF5DatasetGenerator:
     def __init__(self, dbPath, batchSize, preprocessors=None,
         aug=None, binarize=True, classes=2):
-        # store the batch size, preprocessors, and data augmentor,
-        # whether or not the labels should be binarized, along with
-        # the total number of classes
         self.batchSize = batchSize
         self.preprocessors = preprocessors
         self.aug = aug
         self.binarize = binarize
         self.classes = classes
 
-        # open the HDF5 database for reading and determine the total
-        # number of entries in the database
         self.db = h5py.File(dbPath)
         self.numImages = self.db["labels"].shape[0]
 
     def generator(self, passes=np.inf):
-        # initialize the epoch count
+        # 初始化epoch
         epochs = 0
 
-        # keep looping infinitely -- the model will stop once we have
-        # reach the desired number of epochs
+        # 一直循环，直到到达设置的epochs
         while epochs < passes:
-            # loop over the HDF5 dataset
+            # 循环HDF5数据集
             for i in np.arange(0, self.numImages, self.batchSize):
-                # extract the images and labels from the HDF dataset
+                # 提取图像和标签
                 images = self.db["images"][i: i + self.batchSize]
                 labels = self.db["labels"][i: i + self.batchSize]
 
-                # check to see if the labels should be binarized
+                # 看下标签是否二值化
                 if self.binarize:
                     labels = np_utils.to_categorical(labels,
                                                      self.classes)
 
-                # check to see if our preprocessors are not None
+                # 确定是否使用预处理器
                 if self.preprocessors is not None:
-                    # initialize the list of processed images
                     procImages = []
                     
-                    # loop over the images
+                    # 循环图像
                     for image in images:
-                        # loop over the preprocessors and apply each
-                        # to the image
                         for p in self.preprocessors:
                             image = p.preprocess(image)
                         
-                        # update the list of processed images
                         procImages.append(image)
                     
-                    # update the images array to be the processed
-                    # images
                     images = np.array(procImages)
 
-                # if the data augmenator exists, apply it
+                # 应用数据增强
                 if self.aug is not None:
                     (images, labels) = next(self.aug.flow(images,
                                                           labels, batch_size=self.batchSize))
 
-                # yield a tuple of images and labels
+                # 以元组格式返回图像和标签
                 yield (images, labels)
                 
-            # increment the total number of epochs
+            # 递增epochs
             epochs += 1
             
     def close(self):
-        # close the database
+        # 关闭数据集
         self.db.close()
-
-
-
